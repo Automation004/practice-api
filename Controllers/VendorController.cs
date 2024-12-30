52390694d986b986b06730c80ed27243be3aebf4
Sharuk <sharuk.komminapalli@audreeinfotech.com>
2024-12-30 12:18:32 +0530

    first commit

---
+++ Controllers/VendorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Audree.Practice.Api.Controllers
+{
+    public class VendorController : Controller
+    {
+        public IActionResult GetAction()
+        {
+            return View();
+        }
+    }
+}
